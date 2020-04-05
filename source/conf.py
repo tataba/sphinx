7af858bee80d50c174559c4c43970260fbea2f23
wangyq <wangyq@onbonbx.com>
2020-04-05 13:27:43 +0800

    add my.css, update conf.py

--- source/conf.py
+++ source/conf.py
@@ -71,7 +71,8 @@ release = recommonmark.__version__
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "zh_CN"
+
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
@@ -145,6 +146,8 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = ['css/my.css']
+
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
